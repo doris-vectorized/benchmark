f4d019e09e751e311a2c4494e6314ff3c0217658
fenghaoan <fenghaoan@baidu.com>
2021-03-17 19:22:51 +0800

    add some code
    Change-Id: Ifba30793d9f3285431efb80d59275b8da9e57597

---
+++ build.sh
@@ -0,0 +1,17 @@
+source ./env.sh
+BASE=`pwd`
+INSTALL=`pwd`/thirdparty/install
+PARALLEL=56
+
+#cd thirdparty/boost_1_73_0
+#./bootstrap.sh --prefix=$INSTALL
+#./b2 link=static runtime-link=static -j$PARALLEL --without-mpi --without-graph --without-graph_parallel --without-python cxxflags="-std=c++11 -g -fPIC" install
+
+#cd $BASE
+#cd thirdparty/googletest
+#mkdir build;cd build;cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL;make -j $PARALLEL;make install
+
+cd $BASE
+mkdir build;
+cd build;
+cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j$PARALLEL 
---
+++ env.sh
@@ -0,0 +1,3 @@
+export CC=/opt/compiler/gcc-8.2/bin/gcc
+export CXX=/opt/compiler/gcc-8.2/bin/g++
+export PATH=/opt/compiler/gcc-8.2/bin:$PATH
