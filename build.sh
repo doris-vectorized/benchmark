0ef9e009470edbea9f83058d07a4f86dbbe53b99
lihaopeng <lihaopeng@baidu.com>
2021-03-17 20:26:59 +0800

    compile no problem
    Change-Id: I734fc214f69ef4c14d2fc90188d94b0c5fd31309

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ PARALLEL=56
 #./bootstrap.sh --prefix=$INSTALL
 #./b2 link=static runtime-link=static -j$PARALLEL --without-mpi --without-graph --without-graph_parallel --without-python cxxflags="-std=c++11 -g -fPIC" install
 
-#cd $BASE
-#cd thirdparty/googletest
-#mkdir build;cd build;cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL;make -j $PARALLEL;make install
+cd $BASE
+cd thirdparty/googletest-release-1.8.0
+mkdir build;cd build;cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL;make -j $PARALLEL;make install
 
 cd $BASE
 mkdir build;
