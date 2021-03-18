ef56cc18cf8fbcceceb4d1c2d40b8fa9f0d76142
lihaopeng <lihaopeng@baidu.com>
2021-03-18 11:40:42 +0800

    add IColumn in vec
    Change-Id: I0d0fbf2b8eebf2efa713a8b3b3439b505161487f

--- build.sh
+++ build.sh
@@ -7,9 +7,15 @@ PARALLEL=56
 #./bootstrap.sh --prefix=$INSTALL
 #./b2 link=static runtime-link=static -j$PARALLEL --without-mpi --without-graph --without-graph_parallel --without-python cxxflags="-std=c++11 -g -fPIC" install
 
-cd $BASE
-cd thirdparty/googletest-release-1.8.0
-mkdir build;cd build;cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL;make -j $PARALLEL;make install
+#cd $BASE
+#cd thirdparty/googletest-release-1.8.0
+#mkdir build;cd build;cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL;make -j $PARALLEL;make install
+
+#cd $BASE
+#cd thirdparty/cityhash102
+#mkdir build;cd build;cmake .. ;make -j $PARALLEL
+#cp libcityhash.a $INSTALL/lib
+#cp ../include/* $INSTALL/include
 
 cd $BASE
 mkdir build;
