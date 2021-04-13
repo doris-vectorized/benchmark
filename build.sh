c5c384ecf0254192eee39a6f0fbe6171ba4c91cc
fenghaoan <fenghaoan@baidu.com>
2021-04-13 20:38:33 +0800

    fix some warning
    Change-Id: I642f24f157408cd8a34b2da0fec576a5287a2303

--- build.sh
+++ build.sh
@@ -1,25 +1,8 @@
 source ./env.sh
 BASE=`pwd`
-INSTALL=/home/happen/baidu/bdg/doris/thirdparty/installed
 PARALLEL=56
 
-#cd thirdparty/boost_1_73_0
-#./bootstrap.sh --prefix=$INSTALL
-#./b2 link=static runtime-link=static -j$PARALLEL --without-mpi --without-graph --without-graph_parallel --without-python cxxflags="-std=c++11 -g -fPIC" install
-
-#cd $BASE
-#cd thirdparty/googletest-release-1.8.0
-#mkdir build;cd build;cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL;make -j $PARALLEL;make install
-
-#cd $BASE
-#cd thirdparty/cityhash102
-#rm -rf build;mkdir build;cd build;cmake .. ;make -j $PARALLEL
-#cp libcityhash.a $INSTALL/lib
-#cp ../include/* $INSTALL/include
-
-#cd $BASE
-#cd thirdparty/fmt-7.1.3
-#rm -rf build;mkdir build;cd build;cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL;make -j $PARALLEL;make install
+make
 
 cd $BASE
 mkdir build;
