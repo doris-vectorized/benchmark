ada5c20e35a842c84d27292aaf4819be0657504c
lihaopeng <lihaopeng@baidu.com>
2021-04-13 11:13:39 +0800

    1.support function of intDiv 2.revise dispose script for code move
    Change-Id: I0875ffa6d753add35f0447531a3afadf277a5c3d

--- script/add_vec_header_for_clickhouse.sh
+++ script/add_vec_header_for_clickhouse.sh
@@ -2,5 +2,5 @@ header_dir=$(ls ../src/vec)
 
 for header in $header_dir
 do
-	sed -i "s/#include <$header/#include <vec\/$header/g" $1
+	sed -i "s/#include \"$header/#include \"vec\/$header/g" $1
 done
--- script/rename.sh
+++ script/rename.sh
@@ -2,11 +2,12 @@ header=$(echo "$1" | cut -d "/" -f 1,2,3,4)
 name=$(echo "$1" | cut -d "/" -f 5 | sed 's/^[A-Z]/\l&/g' | sed "s/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/_\l&/g")
 
 echo $1
-echo $header/$name.bak
+#echo $header/$name.bak
 
-cp $1 $header/$name.bak
+cp $1 $header/$name
 
-sed -i "s/namespace DB/namespace doris::vectorized/g" $header/$name.bak
-python3 change_include.py $header/$name.bak
+#sed -i "s/namespace DB/namespace doris::vectorized/g" $header/$name.bak
+python3 change_include.py $header/$name
+sh add_vec_header_for_clickhouse.sh $header/$name
 
 
